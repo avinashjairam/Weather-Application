2670eeae3fbf093835caeea75aedaba4522815de
avinashjairam <avinash.jairam@gmail.com>
2016-07-15 18:04:37 -0400

    Added styled bootstrap navbar with about, home, contact links; add terms and conditions link to footer. Fetching weather image from api and displaying it. Added degree and % symbols

--- weather.php
+++ weather.php
@@ -1,102 +1,156 @@
+
 <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+   <link href="./css/customCss.css" rel="stylesheet">
+   <link href="./css/sticky-footer-navbar.css" rel="stylesheet">
 
-<html>
-<head>
-	<title>Weather Forecast</title>
-	<meta charset="utf-8">
+    <title>WEATHER APP</title>
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+   
+   
 
-	
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript"></script>
-	
-</head>
+    <!-- Custom styles for this template -->
+    <link href="./css/sticky-footer-navbar.css" rel="stylesheet">
+
+    
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">WEATHER APP</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>           
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- Begin page content -->
+    <div class="container">
+     <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+
+        <h3>Find Your Local Weather!</h3>
+       
+
+        <div id="custom-search-input">
+          <div class="input-group">
+              <form id="weatherForm" method="post">
+                 <input id="weatherLocation" name="location" type="text" class="search-query form-control" placeholder="Enter city or zipcode" />
+               </form>
+                <span class="input-group-btn">
+                    <button class="btn btn-danger" type="submit" form="weatherForm">
+                        <span class=" glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+          </div>
 
 
+          <div id="WeatherInformation">
+            <div id="weatherDescription">
+              Description:
+            </div>
+            <div id="temp">
+              Current Temp:
+            </div>
+            <div id="minTemp">
+              Min Temp:
+            </div>
+            <div id="maxTemp">
+              Max Temp:
+            </div>
+            <div id="humidity">
+              Humidity:
+            </div>
+          </div>
 
-<body>
-
-
-	<div id="custom-search-input">
-	    <div class="input-group col-md-12">
-	        <input type="text" class="  search-query form-control" placeholder="Search" />
-	        <span class="input-group-btn">
-	            <button class="btn btn-danger" type="button">
-	                <span class=" glyphicon glyphicon-search"></span>
-	            </button>
-	        </span>
-	    </div>
+        </div>
+       </div>
+     </div>
     </div>
 
+    <footer class="footer">
+      <div class="container">
+        <a href="./termsAndConditions.php">Terms and Conditions</a>
+      </div>
+    </footer>
 
 
-	<form id="weatherForm" method="post">
-		<input id="weatherLocation" type="text" name="location">
-		<input type="submit" value="Submit">
-	</form>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript"></script>
+   
+   
 
-	<div id="weatherDescription">
-	</div>
-	<div id="temp">
-	</div>
-	<div id="minTemp">
-	</div>
-	<div id="maxTemp">
-	</div>
-	<div id="humidity">
-	</div>
+    <script>
+      $(document).ready(function(){
+        $("#WeatherInformation").css("display","none");
 
-	
+      });
 
+      $("#weatherForm").submit(function (event){
+        var city=$("#weatherLocation").val();
+        var weatherIcon;
+        var weatherIconUrl;
 
+        city=city.replace(" ","");
 
+        event.preventDefault();
+        //alert(city);
 
-<script>
-	$(document).ready(function(){
+          $.ajax({
+            url:'weatherApp.php',
+            dataType:"json",
+            data:{city:city},
+            type:'POST',
+            success:function(data){
+              if(!data.error){
+                alert(data.Description);
+        
+                weatherIcon=data.Icon;
 
-	});
-
-	$("#weatherForm").submit(function (event){
-		var city=$("#weatherLocation").val();
-		city=city.replace(" ","");
-
-		event.preventDefault();
-		//alert(city);
-
-		$.ajax({
-			url:'weatherApp.php',
-			dataType:"json",
-			data:{city:city},
-			type:'POST',
-			success:function(data){
-				if(!data.error){
-					alert(data.Description);
-					$('#weatherDescription').html(data.Description);
-					$('#temp').html(data.CurrentTemp);
-					$('#minTemp').html(data.MinTemp);
-					$('#maxTemp').html(data.maxTemp);
-					$('#humidity').html(data.Humidity);
-
-				}
-			}
-		});
-
-	});
+                weatherIconUrl="http://openweathermap.org/img/w/"+weatherIcon+".png";
 
 
+                $('#weatherDescription').html(data.Description);
+                $('#weatherDescription').prepend('<img src='+weatherIconUrl +" alt=\"weather icon\"/><br>")
+                $('#temp').html("current temp: " + data.CurrentTemp+"&deg");
+                $('#minTemp').html("min temp: " + data.MinTemp+"&deg");
+                $('#maxTemp').html("max temp: " + data.MaxTemp+"&deg");
+                $('#humidity').html("humidity: " + data.Humidity+"&#37");
 
+                $("#WeatherInformation").css("display","block");
 
-</script>
+              }
+            }
+          });
 
+       });
+    </script>
 
-
-
-<body>
-
-
-
-
-
-
-<html>
+  </body>
+</html>
--- weatherApp.php
+++ weatherApp.php
@@ -1,30 +1,21 @@
 <?php
 
-	//$city="seattle";
 	$city=$_POST['city'];
 
 	$request="http://api.openweathermap.org/data/2.5/weather?APPID=8f7dcad34954743aefc85f3a8c8eb847&q=".$city."&units=imperial";
 	
-	//$request="http://api.openweathermap.org/data/2.5/find?weather?APPID=8f7dcad34954743aefc85f3a8c8eb847&q=".$city."&units=imperial";
-
 	$response=file_get_contents($request);
 	$jsonData = json_decode($response, true);
 
-	// var_dump($jsonData);
-	
- 	// echo "Description".$jsonData['weather'][0]['description']."<br>";
 
- 	
+ 	print json_encode(array("Description" => "{$jsonData['weather'][0]['description']}",
+ 							"Icon" 		  => "{$jsonData['weather'][0]['icon']}",
+ 							"CurrentTemp" => "{$jsonData['main']['temp']}",
+ 							"MaxTemp"	  => "{$jsonData['main']['temp_max']}",
+ 							"MinTemp"	  => "{$jsonData['main']['temp_min']}",
+ 							"Humidity"	  => "{$jsonData['main']['humidity']}"
 
- 	// echo "CurrentTemp".$jsonData['main']['temp']."<br>";
-
- 	// echo "MaxTemp".$jsonData['main']['temp_max']."<br>";
-
- 	// echo "MinTemp".$jsonData['main']['temp_min']."<br>";
-
- 	// echo "Humidity".$jsonData['main']['humidity']."<br>";
-
- 	print json_encode(array("Description" => "{$jsonData['weather'][0]['description']}"));
+ 							 ));
  
 
 
