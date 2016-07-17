6e94c6eeb632bf65cb719a2fb0336e7c6504945b
avinashjairam <avinash.jairam@gmail.com>
2016-07-16 22:12:27 -0400

    About page

---
+++ about.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About</title>
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="weather app">
+    <meta name="Avinash Jairam" content="weather app">
+   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+   <link href="./css/customCss.css" rel="stylesheet">    
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">WEATHER APP</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li class="active"><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>           
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- Begin page content -->
+    <div class="container">
+     <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+
+        <h1>About</h1>
+
+        <p>Weather App is a simple application which takes the user input and queries the <a href="http://openweathermap.org/">openweathermap</a> API to get the relevant weather information. This include the current humidity, temperature, and weather description. An appropriate graphic obtained from the API is also displayed.</p> 
+       
+
+   
+       </div>
+     </div>
+    </div>
+
+    <footer class="footer">
+      <div class="container">
+        <a href="./termsAndConditions.php">Terms and Conditions</a>
+      </div>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript"></script>
+   
+   
+
+   
+
+  </body>
+</html>
