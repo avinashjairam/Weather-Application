34017cc347034dbd079bb37a17ed77b7de730f37
avinashjairam <avinash.jairam@gmail.com>
2016-07-16 22:40:01 -0400

    fixed links in navbar

--- weather.php
+++ weather.php
@@ -37,9 +37,9 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>           
+            <li class="active"><a href="weather.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>           
           </ul>
         </div><!--/.nav-collapse -->
       </div>
